1fd4770e7e1de4f50f49fe6d98387aedcff96e1c
hajikhan <hajikhan110@yahoo.com>
2024-02-26 14:38:48 +0500

    msg

--- 14augarray.js
+++
@@ -1,4 +0,0 @@
-var Guest = ['Kareem', 'Raheem'];
-for (var i = 0; i < Guest.length; i++) {
-    console.log('I welcome you Sir!' + Guest[i]);
-}
--- 14augarray.ts
+++
@@ -1,4 +0,0 @@
-let Guest :string[] = ['Kareem','Raheem'];
-for(let i=0; i<Guest.length;i++){
-    console.log('I welcome you Sir!' + Guest[i]);
-}
